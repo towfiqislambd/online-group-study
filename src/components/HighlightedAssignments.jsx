d5c885d0b91d3fa24a44dad2ae44461482a99ed8
towfiqislambd <towfiqislambd@gmail.com>
2025-01-12 02:45:10 +0600

    added some componets

--- src/components/HighlightedAssignments.jsx
+++ src/components/HighlightedAssignments.jsx
@@ -17,7 +17,7 @@ const HighlightedAssignments = () => {
                 <h2 className="md:text-4xl text-3xl dark:text-white font-semibold text-gray-800 mb-4">Highlighted Assignments</h2>
                 <p className="lg:w-1/2 md:w-2/3 mx-auto dark:text-gray-400 text-gray-600">There are many people which includes homeless, disaster victims, underprivileged and children in need of clothing. We cannot do so much for them but try</p>
             </div>
-            <div className="grid grid-cols-3 gap-5">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
                 {
                     slicedAssignments.map(assignment => <Highlightes key={assignment._id} assignment={assignment}></Highlightes>)
                 }
